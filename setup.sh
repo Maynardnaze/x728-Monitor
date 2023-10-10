0d30dc8648107103a3c799a2dff65cef624a385c
Andrew Maynard <zeroordie895@gmail.com>
2023-10-10 17:25:02 -0400

    First Push

--- setup.sh
+++ setup.sh
@@ -3,7 +3,18 @@ if [ `whoami` != "root" ] || [ "$UID" != 0 ]; then
   echo "I must be run with sudo."
   exit 1;
 fi
-apt-get install python3-smbus i2c-tools python3-rpi.gpio
+cd ~
+git clone https://github.com/geekworm-com/x728-script
+cd x728-script
+chmod +x *.sh
+cp -f ./x728-pwr.sh                /usr/local/bin/
+cp -f ./x728-v2.x-softsd.sh        /usr/local/bin/x728-softsd.sh
+apt-get install -y python3-smbus python3-rpi.gpio
+
+echo "alias x728off='sudo /usr/local/bin/x728-softsd.sh'" >>   ~/.bashrc
+source ~/.bashrc
+
+
 echo -e 'i2c-dev' >> /etc/modules
 modprobe -a i2c-dev
 echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-1/new_device
@@ -13,8 +24,7 @@ sleep 2
 hwclock -w
 
 cp x728.conf /etc/
-cp x728-Monitor.py /usr/bin/x728-Monitor
+cp x728-monitor.py /usr/bin/x728-monitor.py
 cp x728Monitor.service  /etc/systemd/system/
 chmod u+rwx /etc/systemd/system/x728Monitor.service
-systemctl enable x728Monitor.service
-systemctl start x728Monitor.service
+systemctl enable --now x728Monitor.service
